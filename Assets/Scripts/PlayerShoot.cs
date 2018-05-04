28b058a7f09cb0beab84f7e5a0310bbab89cfae9
BMK\asu <asu@bmk.by>
2018-05-04 17:14:02 +0300

    work commit

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour {
+    public Weapon weapon;
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    public LayerMask mask;
+     void Start()
+    {
+        if (cam == null)
+        { Debug.LogError("No camera"); }
+    }
+     void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit _hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit,weapon.range, mask)){ Debug.Log("We did shot a " + _hit.collider.name); }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Weapon  {
+
+    public string name = "Pistol";
+    public float damage = 10f;
+    public float range = 100f;
+
+}
